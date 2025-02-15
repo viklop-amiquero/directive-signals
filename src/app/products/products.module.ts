91fff7fcfcc54b447493dd9b4e1fcca5f8935719
viklop <viklop7025@gmail.com>
2025-02-15 13:15:32 -0500

    feat: configurar rutas y crear el formulario

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { ProductsRoutingModule } from './products-routing.module'
+import { ProductPageComponent } from './pages/product-page/product-page.component'
+import { ReactiveFormsModule } from '@angular/forms'
+
+@NgModule({
+    declarations: [ProductPageComponent],
+    imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
+})
+export class ProductsModule {}
