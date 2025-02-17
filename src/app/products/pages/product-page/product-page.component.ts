deb0b456f0db9bd8d621020bdb0eccb1671f3ffd
viklop <viklop7025@gmail.com>
2025-02-17 13:00:04 -0500

    feat: Crear formulario de productos

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
-  selector: 'app-product-page',
-  standalone: false,
-  templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.css'
+    selector: 'app-product-page',
+    standalone: false,
+    templateUrl: './product-page.component.html',
+    styleUrl: './product-page.component.css',
 })
 export class ProductPageComponent {
+    private _fb = inject(FormBuilder)
+    public color: string = 'green'
 
+    public myForm: FormGroup = this._fb.group({
+        name: [
+            '',
+            [Validators.required, Validators.minLength(6), Validators.email],
+        ],
+    })
+
+    changeColor() {
+        this.color = '#xxxxxx'.replace(/x/g, (y) =>
+            ((Math.random() * 16) | 0).toString(16)
+        )
+    }
 }
